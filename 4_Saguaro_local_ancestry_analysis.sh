696ffe9a2e42daf18ebde39c62ea4e918f0130f1
Joanna Kelley <joanna.l.kelley@gmail.com>
2021-08-18 11:26:03 -0700

    Update 4_Saguaro_local_ancestry_analysis.sh
    Added lines 20-27 to clarify summarizing the data

--- 4_Saguaro_local_ancestry_analysis.sh
+++ 4_Saguaro_local_ancestry_analysis.sh
@@ -15,3 +15,13 @@ VCF2HMMFeature -i merged.vcf.gz \
 # Run Saguaro, -f indicates the input HMM file, -o indicates the output directory, -iter indicates the number of iterations to go through (29 results in 30 cacti)
 
 Saguaro -f merged.saguaro -o <path to desired output directory> -iter 29
+
+
+Saguaro2Phylip -i saguaro.cactus -outgroup 1
+
+# Need to download Phylip (I use the GUI, http://evolution.genetics.washington.edu/phylip.html) to use the Neighbor module to go from Phylip files to neighbor joining trees
+# The final output should be trees in newick format
+
+# Need to identify which regions are best described to which cacti
+
+grep score LocalTrees.out cactus_locations.txt
